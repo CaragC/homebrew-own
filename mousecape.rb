4b42b8ca74c76303594585d86e40c49a8d059aa1
CaragC <70698008+CaragC@users.noreply.github.com>
2022-05-30 19:22:19 +0200

    Add files via upload

---
+++ mousecape.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Mousecape < Formula
+  desc "Cursor Manager for OSX"
+  homepage "http://www.alexzielenski.com"
+  url "https://github.com/alexzielenski/Mousecape/archive/refs/tags/1813.tar.gz"
+  sha256 "a009f58d08951886d8cbbdd0cb8cacb1ff8dd746f00abdab6a3fab168d1ff583"
+  license "NOASSERTION"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Mousecape`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
