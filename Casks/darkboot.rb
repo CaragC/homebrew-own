1bbb75caa9aebe846964ceb5b4166ba6acc9cbed
CaragC <70698008+CaragC@users.noreply.github.com>
2022-06-03 20:15:42 +0200

    Update darkboot.rb

--- Casks/darkboot.rb
+++ Casks/darkboot.rb
@@ -1,11 +1,11 @@
 cask "darkboot" do
   version "7.0.1"
-  sha256 "7c3b6bae532ffad57c35d93385153f5865bc801fafecce1bb2aa05c210b2a7ca"
+  sha256 "c17c036524aa70afc1f92a9f24e86ef06710dbf05df89e1ac8a847f317d53426"
 
-  url "https://github.com/w0lfschild/app_updates/raw/master/DarkBoot/DarkBoot_#{version}.zip"
+  url "https://github.com/w0lfschild/app_updates/blob/master/DarkBoot/DarkBoot_#{version}.zip"
   name "darkboot"
-  desc "Darkboot"
-  homepage "https://www.macenhance.com/bootbuddy.html"
+  desc "DarkBoot"
+  homepage ""
 
   app "Dark Boot.app"
 end
