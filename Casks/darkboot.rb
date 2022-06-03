ff58c3a74098ffb4d39257bcdd794afc61a3fd48
CaragC <70698008+CaragC@users.noreply.github.com>
2022-06-03 20:09:27 +0200

    Update darkboot.rb

--- Casks/darkboot.rb
+++ Casks/darkboot.rb
@@ -1,6 +1,6 @@
 cask "darkboot" do
   version "7.0.1"
-  sha256 "893b752177eac69e620a4ec660cee165f90ac780807c9d0c784d9a4bfce475eb"
+  sha256 "58f3ee612b2fe796a6b86ca62ec3f71147885a8a5178571133b259c8e1c00f7d"
 
   url "https://github.com/w0lfschild/app_updates/raw/master/DarkBoot/DarkBoot_#{version}.zip"
   name "darkboot"
